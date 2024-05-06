083e689c56eb5283767e034436ba39f6578e9c46
filip <fcobanin@gmail.com>
2024-05-06 15:27:29 +0200

    .

---
+++ Vezbanje/OOP-JavaFX/src/main/java/app/MainKlasa.java
@@ -0,0 +1,15 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainKlasa extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
